8cd0de2cc973747492b8a7ae367c024fddc9b641
Hugh Rawlinson <hugh@spotify.com>
2017-04-17 17:46:00 +0100

    Initial Commit

---
+++ learn-spotify-ios-sdk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learn-spotify-ios-sdk
+//
+//  Created by Hugh Rawlinson on 2017-04-17.
+//  Copyright © 2017 Hugh Rawlinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
